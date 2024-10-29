b9938e71bd48197b09832248cb290ac900140a11
Nicholas-Fearon <nejfearon@gmail.com>
2024-10-29 15:12:12 +0000

    multi page

---
+++ client/app.js
@@ -0,0 +1,10 @@
+const startBtn = document.getElementById("startBtn");
+const option1 = document.getElementById("Option1");
+const option2 = document.getElementById("Option2");
+
+/* get funtion to get:
+room description
+option 1
+option 2
+*/
+

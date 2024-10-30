1ef87386d01d397fdfd5ffb5dce999ac75487d70
Nicholas-Fearon <nejfearon@gmail.com>
2024-10-30 14:54:58 +0000

    nick6

--- client/page2.js
+++ client/page2.js
@@ -5,8 +5,8 @@ document.getElementById("menuButton").addEventListener("click", () => {
 });
 
 const userName = document.getElementById("username");
-const option1 = document.getElementById("Option1");
-const option2 = document.getElementById("Option2");
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
 const area = document.getElementById("area");
 const prompt = document.getElementById("prompt");
 const validation = document.getElementById("validation");
@@ -27,6 +27,9 @@ const getOptions = async () => {
   const res = await fetch("http://localhost:8080/options");
   const data = await res.json();
   console.log(data);
+  area.textContent = data[0].location;
+  option1.textContent = data[0].optionone;
+  option2.textContent = data[0].optiontwo;
 };
 
 // get funtion: outcomes

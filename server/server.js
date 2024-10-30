d61b410ee48f221de0eb965bdb57352265f7b973
adam pink <adampink99@gmail.com>
2024-10-30 10:00:33 +0000

    getting

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+//assets all installed and imported
+//server currently running on port 8080 all working
+
 import express from "express";
 import cors from "cors";
 import pg from "pg";
@@ -8,8 +11,10 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-// db pool url goes here
+// db pool url set up here, password and url all connected
 const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+
+//prompts now all connected on the server api for referance
 
 app.get("/gameprompts", async function (request, response) {
   const result = await db.query("SELECT * FROM gameprompts");
@@ -17,13 +22,37 @@ app.get("/gameprompts", async function (request, response) {
   response.json(gameprompts);
 });
 
+app.get("/outcomes", async function (request, response) {
+  const result = await db.query("SELECT * FROM outcomes");
+  const outcomes = result.rows;
+  response.json(outcomes);
+});
+
+app.get("/monsters", async function (request, response) {
+  const result = await db.query("SELECT * FROM monsters");
+  const monsters = result.rows;
+  response.json(monsters);
+});
+
+app.get("/room", async function (request, response) {
+  const result = await db.query("SELECT * FROM room");
+  const room = result.rows;
+  response.json(room);
+});
+
+app.get("/username", async function (request, response) {
+  const result = await db.query("SELECT * FROM username");
+  const username = result.rows;
+  response.json(username);
+});
+// empty space atm , need to decide on options and outcomes for each then can construct the post functions to data base and server api
 app.get("/options", async function (request, response) {
   const result = await db.query("SELECT * FROM options");
   const options = result.rows;
   response.json(options);
 });
 
-//test
+//test, runs fine
 app.get("/", function (request, response) {
   response.json("this is the root test");
 });
